1c49db0d2fd21d61509f12c291c425a33b02ca50
fendilaw41 <fendilaw41@gmail.com>
2021-05-06 09:44:07 +0700

    fendi

--- src/firebase-example.js
+++ src/firebase-example.js
@@ -1,20 +1,19 @@
-
 import firebase from '@firebase/app'
 import 'firebase/firestore'
 import 'firebase/firebase-auth'
 import 'firebase/storage'
-  // Initialize Firebase
-  var config = {
-    apiKey: "yourKey",
-    authDomain: "yourDomein.firebaseapp.com",
-    databaseURL: "https://yourDomein.firebaseio.com",
-    projectId: "yourDomein",
-    storageBucket: "yourDomein.appspot.com",
-    messagingSenderId: "34543545"
-  };
-  
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBQnph6ameSMZxup0HtLYwzMvOGd0pr1yE",
+    authDomain: "vue-shop-dc41f.firebaseapp.com",
+    databaseURL: "https://vue-shop-dc41f-default-rtdb.firebaseio.com",
+    projectId: "vue-shop-dc41f",
+    storageBucket: "ygs://vue-shop-dc41f.appspot.com",
+    messagingSenderId: "887191639749"
+};
+
 const fb = firebase.initializeApp(config);
 
 const db = firebase.firestore();
 
-export {fb,db}
+export { fb, db }
